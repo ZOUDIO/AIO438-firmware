bce2cec2a162c92c3ced741deccb3102478075e9
ZOUDIO <info@zoudio.com>
2022-01-06 23:55:02 +0100

    AIO438-firmware: New eeprom layout draft

--- Firmware/AIO438-firmware/eepromLayout.h
+++ Firmware/AIO438-firmware/eepromLayout.h
@@ -1,37 +1,50 @@
-enum entryType {
-  empty,
-  factoryEntry,
-  userEntry,
-  dspEntry
+enum class entryType { //Determines how the entry data is handled
+  system_variables, 
+  dsp_default,  //Always used
+  dsp_eq_off,   //Only used when EQ is disabled
+  dsp_eq_on,    //Only used when EQ is enabled
+  empty = 255,  //255 is the default value of an eeprom byte
 };
 
-struct entry {
+struct entryStruct {
   entryType type;
-  uint16_t offset;
-  uint16_t size;
+  uint16_t location;
+  uint16_t size; 
   uint16_t crc;
-  uint8_t amp; //0 = all
-  uint8_t rfu[4];
+  uint8_t amp; //0 = all amps
 };
 
-struct versionStruct {
-  byte major;
-  byte minor;
-  byte patch;
+struct allocationTable {
+  entryStruct entry[32];
 };
 
-struct factoryDataStruct {
-  versionStruct hwVersion;
-  versionStruct btVersion;
-};
-
-struct userData {
+struct system_variables {
+  uint8_t volSaveEnable;
+  float volSave;        //Non-volatile place to remember volume level between power cycles
   float volStart;
   float volMax;
   float powerLow;
   float powerShutdown;
 };
 
-struct variables { //fileAllocationTable
-  entry file[32];
+struct versionStruct{
+  byte major;
+  byte minor;
+  byte patch;
+};
+
+struct factoryData {
+  versionStruct hwVersion;
+  versionStruct btVersion;
+  uint8_t rfu[122];
+};
+
+struct eepromLayout {
+  factoryData factory;
+  allocationTable table;
+  //Remainder of eeprom is reserved for data storage
+};
+
+struct variables {
+  //Currently empty, but needed
 };
